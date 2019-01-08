909daf9dc24921fcc59d7025c7aeb795c3c96424
s172189 <s172189@student.dtu.dk>
2019-01-08 10:03:05 +0100

    added plus lul

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String cmd = args[0];
+        int n1 = Integer.parseInt(args[1]);
+        int n2 = Integer.parseInt(args[3]);
+
+
+        switch(cmd){
+            case "plus":
+                System.out.println(n1 + n2);
+        }
     }
 }
