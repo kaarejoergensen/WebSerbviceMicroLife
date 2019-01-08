470ce6956e1268ef3d5508e62e356360ccd37761
Kåre Joakim Jørgensen <s144852@student.dtu.dk>
2019-01-08 10:26:22 +0100

    Fixed gitignore and added maven

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String cmd = args[0];
+        int n1 = Integer.parseInt(args[1]);
+        int n2 = Integer.parseInt(args[2]);
+
+        switch(cmd){
+            case "plus":
+                System.out.println(n1 + n2);
+        }
+    }
+}
