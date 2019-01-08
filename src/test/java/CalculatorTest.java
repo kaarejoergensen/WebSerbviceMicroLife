02987a98283f69a60bc1a1e186b3ebf4d25f3923
Kåre Jørgensen <kaare1994@gmail.com>
2019-01-08 13:30:10 +0100

    mini

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
 
     @Test
+    public void MiniTest() {
+        assertThat("a", is("a"));
+    }
+
+    @Test
     public void TestCalculator() {
         assertThat(Calculator.Add("1,2"), is(3));
     }
