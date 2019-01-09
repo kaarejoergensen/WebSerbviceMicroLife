b3ad1f614158f275d4809f1bed2db4b46a1c534f
Kåre Jørgensen <kaare1994@gmail.com>
2019-01-09 14:46:53 +0100

    Added a QR Generator

--- barcode/src/main/java/barcode/BarcodeProvider.java
+++ barcode/src/main/java/barcode/BarcodeProvider.java
@@ -1,12 +1,24 @@
 package barcode;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import exceptions.QRException;
+import org.apache.commons.lang3.RandomStringUtils;
 import persistence.Datastore;
 import tokens.TokenProvider;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BarcodeProvider {
+    private static final int QR_SIZE = 300;
+
     private TokenProvider tokenProvider;
     private Datastore datastore;
 
@@ -15,14 +27,18 @@ public class BarcodeProvider {
         this.datastore = datastore;
     }
 
-    public List<String> getTokens(String userName, String userId, int numberOfTokens) {
+    public List<String> getTokens(String userName, String userId, int numberOfTokens) throws QRException {
         if (numberOfTokens < 1 || numberOfTokens > 5)
             throw new IllegalArgumentException("The number of tokens must be between 1 and 5");
         int numberOfUnusedTokens = this.datastore.getNumberOfUnusedTokens(userName);
         if (numberOfUnusedTokens > 1)
             throw new IllegalArgumentException("Number of unused tokens is more than 1");
         List<String> tokens = new ArrayList<>();
-        for (int i = 0; i < numberOfTokens; i++) tokens.add(this.tokenProvider.issueToken(userName, userId));
+        for (int i = 0; i < numberOfTokens; i++) {
+            String token = this.tokenProvider.issueToken(userName, userId);
+            tokens.add(token);
+            this.generateQRCode(token);
+        }
         this.datastore.addTokens(tokens.size(), userName);
         return tokens;
     }
@@ -32,5 +48,20 @@ public class BarcodeProvider {
         if (tokenValid)
             this.datastore.useToken(tokenString);
         return tokenValid;
+    }
+
+    private void generateQRCode(String tokenString) throws QRException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        String directory = System.getProperty("user.dir") + "/images/";
+        String randomString = RandomStringUtils.randomAlphabetic(10);
+        Path path = FileSystems.getDefault().getPath(directory + randomString+ ".png");
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = qrCodeWriter.encode(tokenString, BarcodeFormat.QR_CODE, QR_SIZE, QR_SIZE);
+            MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+        } catch (WriterException | IOException e) {
+            throw new QRException(e.getMessage(), e);
+        }
+
     }
 }
--- barcode/src/test/java/BarcodeTest.java
+++ barcode/src/test/java/BarcodeTest.java
@@ -1,13 +1,20 @@
 import barcode.BarcodeProvider;
+import exceptions.QRException;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import persistence.Datastore;
 import persistence.MemoryDataStore;
 import tokens.TokenProvider;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class BarcodeTest {
@@ -26,7 +33,7 @@ public class BarcodeTest {
     }
 
     @Test
-    public void issueTokens() {
+    public void issueTokens() throws QRException {
         List<String> tokens = barcodeProvider.getTokens(this.userName, this.userId, 5);
         assertThat(tokens, is(notNullValue()));
         assertThat(tokens.size(), is(5));
@@ -41,7 +48,7 @@ public class BarcodeTest {
     }
 
     @Test
-    public void tamperedToken() {
+    public void tamperedToken() throws QRException {
         List<String> tokens = barcodeProvider.getTokens(this.userName, this.userId, 1);
         assertThat(tokens, is(notNullValue()));
         assertThat(tokens.size(), is(1));
@@ -49,17 +56,17 @@ public class BarcodeTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void maxFiveTokens() {
+    public void maxFiveTokens() throws QRException {
         barcodeProvider.getTokens(this.userName, this.userId, 6);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void atLeastOneToken() {
+    public void atLeastOneToken() throws QRException {
         barcodeProvider.getTokens(this.userName, this.userId, 0);
     }
 
     @Test
-    public void useToken() {
+    public void useToken() throws QRException {
         List<String> tokens = barcodeProvider.getTokens(this.userName, this.userId, 1);
         assertThat(tokens, is(notNullValue()));
         assertThat(tokens.size(), is(1));
@@ -70,7 +77,7 @@ public class BarcodeTest {
     }
 
     @Test
-    public void getTokenIfOnlyOneUnused() {
+    public void getTokenIfOnlyOneUnused() throws QRException {
         List<String> tokens = barcodeProvider.getTokens(this.userName, this.userId, 1);
         tokens.addAll(barcodeProvider.getTokens(this.userName, this.userId, 1));
         assertThat(tokens, is(notNullValue()));
@@ -80,10 +87,16 @@ public class BarcodeTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void maxOneUnusedToken() {
+    public void maxOneUnusedToken() throws QRException {
         barcodeProvider.getTokens(this.userName, this.userId, 2);
         assertThat(datastore.getNumberOfUnusedTokens(this.userName), is(2));
         barcodeProvider.getTokens(this.userName, this.userId, 1);
     }
 
+    @AfterClass
+    public static void cleanUpQRCodes() throws IOException {
+        String directory = System.getProperty("user.dir") + "/images/";
+        Path path = FileSystems.getDefault().getPath(directory);
+        Files.walk(path).map(Path::toFile).filter(f -> !f.isDirectory()).forEach(System.out::println);
+    }
 }
